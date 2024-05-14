99d25437a232cafa3dbfb1f49d2b6ef15e15cc4f
Prakash Gurung <prakashgrg4400@gmail.com>
2024-05-15 00:37:09 +0545

    hide and show animations

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ console.log(heading);
 let buttons = document.querySelectorAll(".side-nav .animate-buttons button");
 console.log(buttons);
 
+const showAnimations = document.querySelector(".show-animations");
+
 buttons.forEach((button) => {
   button.addEventListener("click", handleAnimation);
 });
@@ -57,6 +59,9 @@ function handleToggle(event) {
     sun.style.color = "#E0E0E0";
     btnHeader.style.color = "#E0E0E0";
     sidenav.style.backgroundColor = "#15151D";
+    showAnimations.style.backgroundColor = "#15151D";
+    showAnimations.style.color = "#E0E0E0";
+    showAnimations.style.borderColor = "#E0E0E0";
     buttons.forEach((button) => {
       button.style.color = "#E0E0E0";
     });
@@ -68,8 +73,27 @@ function handleToggle(event) {
     moon.style.color = "#000000";
     btnHeader.style.color = "#000000";
     sidenav.style.backgroundColor = "#F7D7B5";
+    showAnimations.style.backgroundColor = "transparent";
+    showAnimations.style.color = "#351C75";
+    showAnimations.style.borderColor = "#351C75";
     buttons.forEach((button) => {
       button.style.color = "#000000";
     });
+  }
+}
+
+//!============ handeling show and hide of side nav =====================
+console.log(showAnimations);
+showAnimations.addEventListener("click", handleShowAnimation);
+
+function handleShowAnimation(event) {
+  if (event.target.textContent === "See animations") {
+    sidenav.style.right = "0px";
+    section.style.width = "calc(100vw - 300px)";
+    event.target.textContent = "Close animations";
+  } else {
+    sidenav.style.right = "-300px";
+    section.style.width = "100vw";
+    event.target.textContent = "See animations";
   }
 }
